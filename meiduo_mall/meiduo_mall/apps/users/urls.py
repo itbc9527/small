95210f5951bce38d6e229fd8ad8914f099f097ee
baochao <731681815@qq.com>
2018-10-26 14:32:44 +0800

    初始化

---
+++ meiduo_mall/meiduo_mall/apps/users/urls.py
@@ -0,0 +1,6 @@
+# -*- coding:utf-8 -*-
+# @File  :urls.py
+# @Author:bc
+# @Date  :18-10-26
+# @Desc  :
+urlpatterns = []
---
+++ meiduo_mall/meiduo_mall/settings/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding:utf-8 -*-
+# @File  :__init__.py.py
+# @Author:bc
+# @Date  :18-10-26
+# @Desc  :
